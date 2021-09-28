1920d4f78902426b024ac7c64525621a5779a3f6
Ahmad-AbuRumuh <ahmadromoh@gmail.com>
2021-09-28 23:50:38 +0300

    lab 12

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose')
+
+const server = express();
+server.use(cors());
+const PORT = process.env.PORT;
+
+mongoose.connect('mongodb://localhost:27017/cats7');
